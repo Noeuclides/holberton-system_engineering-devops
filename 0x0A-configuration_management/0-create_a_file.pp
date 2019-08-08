53f89a64439e2e4fcc5b90b8ce34c61fd213d0eb
Noeuclides <euclidesnoeuclides@gmail.com>
2019-08-08 03:40:27 +0000

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,9 @@
 #create a file in /tmp
-class absent_file {
-
-      file { "/tmp/holberton":
-      	   ensure => "present",
-    	   replace => "no",
-    	   owner => "www-data",
-    	   group => "www-data",
-    	   content => "I love Puppet",
-    	   mode => "0744",
-	}
-
+file { "/tmp/holberton":
+     ensure => "present",
+     replace => "no",
+     owner => "www-data",
+     group => "www-data",
+     content => "I love Puppet",
+     mode => "0744",
 }
