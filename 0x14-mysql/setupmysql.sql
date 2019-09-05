680e0a61829d09cb7f0472ad892abcfb3bc908bb
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-05 18:36:06 +0000

    create database and table

--- 0x14-mysql/setupmysql.sql
+++ 0x14-mysql/setupmysql.sql
@@ -2,4 +2,9 @@
 DROP USER IF EXISTS 'holberton_user'@'localhost';
 CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+GRANT SELECT ON *.* TO 'holberton_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE `nexus6` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(127) NOT NULL, PRIMARY KEY (`id`));
+INSERT INTO `nexus6` (name) VALUES ("Ernesto");
 
