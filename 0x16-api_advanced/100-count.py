81b60b8d1d2a0b9a90e8d47f3d0f148641a00e01
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-20 13:12:13 +0000

    reddit aPI 2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""queries the Reddit API and returns a list containing the titles
+of all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """return list of all articles
+    """
+    url_base = 'https://www.reddit.com/' + 'r/' + subreddit + '/hot.json'
+    head = {"user-agent": 'N'}
+    r = requests.get(url_base, headers=head, allow_redirects=False)
+    if r.status_code != 200:
+        print(None)
+    else:
+        data = r.json().get('data')
+        child = data.get('children')
+        for i in range(10):
+            print(child[i].get('data').get('title'))
