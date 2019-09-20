7745f181b6d9b10a23c828f649d66b7bd22acf66
Noeuclides <euclidesnoeuclides@gmail.com>
2019-09-20 04:34:08 +0000

    reddit aPI 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
+"""query Reddit API and print tittles of first ten listed
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """print top ten listed
+    """
+    url_base = 'https://www.reddit.com/' + 'r/' + subreddit + '/hot.json'
+    head = {"user-agent": 'N'}
+    r = requests.get(url_base, headers=head, allow_redirects=False)
+    if r.status_code != 200:
+        print(None)
+    else:
+        data = r.json().get('data')
+        child = data.get('children')
+        for i in range(10):
+            print(child[i].get('data').get('title'))
