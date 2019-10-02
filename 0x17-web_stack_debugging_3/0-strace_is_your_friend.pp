e343f56e204649d0e11def1c18a5a33f370194e8
Noeuclides <euclidesnoeuclides@gmail.com>
2019-10-02 20:34:48 +0000

    quotes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Web stack debugging
 exec { '/var/www/html/wp-setting.php':
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php',
+  command => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
 }
