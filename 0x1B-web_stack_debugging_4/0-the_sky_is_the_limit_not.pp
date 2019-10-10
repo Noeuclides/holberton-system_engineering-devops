44bd83e815a700baa273a61942e28c6afc89bffd
Noeuclides <euclidesnoeuclides@gmail.com>
2019-10-10 21:26:09 +0000

    web debuggin

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,12 +5,6 @@ exec { '/etc/default/nginx':
 }
 
 exec { '/etc/security/limits.conf':
+  command => 'sudo service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed $ a "# Added Nginx limits\nnginx       soft    nofile  30000\nnginx       hard    nofile  50000" /etc/security/limits.conf',
 }
-
-#exec { '/etc/nginx/nginx.conf':
-#  path    => '/usr/bin:/usr/sbin:/bin',
-#  command => 'sed '4 a # 2019-10-09 Increase open files' /etc/nginx/nginx.conf',
-#  command => 'sed '5 a worker_rlimit_nofile 30000;' /etc/nginx/nginx.conf',
-#}
