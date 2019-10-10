7b1e1bc3471a073ffa1a0b2844ff0636947986cc
Noeuclides <euclidesnoeuclides@gmail.com>
2019-10-10 21:30:15 +0000

    sequencial ->

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,8 +2,7 @@
 exec { '/etc/default/nginx':
   path    => '/usr/bin:/usr/sbin:/bin',
   command => 'sed -i s/15/4096/g /etc/default/nginx',
-}
-
+}->
 exec { '/etc/security/limits.conf':
   command => 'sudo service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
